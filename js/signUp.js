75c072407df11d54e3be016ba0b0f30b1110f9c2
MoatazNasser <103342321+MoatazNasser@users.noreply.github.com>
2022-10-09 16:17:48 +0200

    Update signUp.js

--- js/signUp.js
+++ js/signUp.js
@@ -156,8 +156,8 @@ getIPs().then((res) => {
   
 var result = JSON.stringify(res[0]) ;
   alert(result);
-  result = result.split(",");
-  result = result[0];
+//   result = result.split(",");
+//   result = result[0];
   alert(result);
   result = result.slice(1, result.length - 1);
   alert(result);
